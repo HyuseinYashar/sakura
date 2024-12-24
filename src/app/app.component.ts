c084ad290803640be7c9f925ab775d8807b02092
Hyusein Yashar <hyashar96@gmail.com>
2024-12-24 19:32:45 +0100

    see app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,12 @@ import { landingPageComponent } from './main-content/landingPage/landingPage.com
 import { NavbarComponent } from './main-content/landingPage/navbar/navbar.component';
 import { CommonModule } from '@angular/common';
 import { MainContentComponent } from './main-content/main-content.component';
+import { FooterComponent } from './footer/footer.component';
+import { ImprintComponent } from './imprint/imprint.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule,RouterOutlet, MainContentComponent],
+  imports: [CommonModule,RouterOutlet, MainContentComponent,FooterComponent,ImprintComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
